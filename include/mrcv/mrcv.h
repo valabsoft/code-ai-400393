d08440f9830f7dd5340f9d8482a0bd22037a8d67
Vasiliy Alchakov <alchakov@gmail.com>
2024-07-25 15:29:47 +0300

    Added dependency on OpenCV. mrcv-imread added

--- include/mrcv/mrcv.h
+++ include/mrcv/mrcv.h
@@ -2,7 +2,14 @@
 
 #include <mrcv/export.h>
 
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
 namespace mrcv
 {
 	MRCV_EXPORT int add(int a, int b);
+	MRCV_EXPORT int imread(std::string pathtoimage);
 }
