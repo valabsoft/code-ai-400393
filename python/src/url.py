aaaf2055dc9f6e824786047f1a9e006ed368956a
Олег <rolets@yandex.ru>
2024-07-27 14:03:24 +0300

    Функция передает страницу в string

--- python/src/url.py
+++ python/src/url.py
@@ -1,13 +1,15 @@
 import requests
-url = 'https://yandex.ru/images/search?from=tabbar&text=пингвины'
+url = 'https://yandex.ru/images/search?from=tabbar&text=самолеты'
  
- 
+
+
 response = requests.get(url)
-file_Path = '../body.html'
+print(response.content)
+#file_Path = '../body.html'
  
-if response.status_code == 200:
-    with open(file_Path, 'wb') as file:
-        file.write(response.content)
-    print('File downloaded successfully')
-else:
-    print('Failed to download file')
+#if response.status_code == 200:
+#    with open(file_Path, 'wb') as file:
+#        file.write(response.content)
+#    print('File downloaded successfully')
+#else:
+#    print('Failed to download file')
