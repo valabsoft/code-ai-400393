26e127361eb760de44e97188f673f8ba72b421b9
Maksim Dimin <dimin.maksim@yandex.ru>
2024-10-10 20:21:53 +0300

    AI-102, AI-106: Изменение абсолютных путей в exampl'ах на относительные, реструктуризация папок с файлами под структуру библиотеки

--- examples/detectorautotrain/main.cpp
+++ examples/detectorautotrain/main.cpp
@@ -2,10 +2,21 @@
 
 int main()
 {
-	std::string rootPath = "D:/Games/mrcv/examples/detectorautotrain/";
+	std::filesystem::path imageFiles("files\\onwater\\");
+	std::filesystem::path vocClassesFile("files\\onwater\\voc_classes.txt");
+	std::filesystem::path modelSaveFile("files\\onwater_autodetector.pt");
+	std::filesystem::path pretrainedModelFile("files\\yolo4_tiny.pt");
+
+	auto currentPath = std::filesystem::current_path();
+
+	auto datasetPath = currentPath / imageFiles;
+	auto vocClassesPath = currentPath / vocClassesFile;
+	auto modelSavePath = currentPath / modelSaveFile;
+	auto pretrainedModelPath = currentPath / pretrainedModelFile;
 
 	mrcv::Detector detector;
-	detector.Initialize(0, 416, 416, rootPath + "onwater/voc_classes.txt");
-	detector.AutoTrain(rootPath + "onwater/", ".jpg", { 10, 15, 30}, { 4, 8 }, {0.001, 1.0E-4F}, rootPath + "yolo4_tiny.pt", rootPath + "onwater_autodetector.pt");
+	detector.Initialize(0, 416, 416, vocClassesPath.string());
+	detector.AutoTrain(datasetPath.u8string(), ".jpg", { 10, 15, 30 }, { 4, 8 }, { 0.001, 1.0E-4F }, pretrainedModelPath.u8string(), modelSavePath.u8string());
+
 	return 0;
 }
