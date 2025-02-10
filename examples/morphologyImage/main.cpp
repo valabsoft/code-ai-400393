2ca12c7ddb41e71e71101417566cc6de71196e9f
Олег <rolets@yandex.ru>
2025-02-10 19:26:03 +0300

    Update main.cpp

--- examples/morphologyImage/main.cpp
+++ examples/morphologyImage/main.cpp
@@ -5,10 +5,6 @@ int main()
     int morph_size = 1;
 
     cv::Mat image = cv::imread("/home/oleg/install/code-ai-400393-developer/examples/morphologyImage/files/opening.png", cv::IMREAD_GRAYSCALE);
-    //image = imread("D:\\QtProect\\closing.png", IMREAD_GRAYSCALE);
-   // image = imread("D:\\QtProect\\gradient.png", IMREAD_GRAYSCALE);
-   // Mat image = imread("files/j.png", IMREAD_GRAYSCALE);
-
     std::string out = "/home/oleg/install/code-ai-400393-developer/examples/morphologyImage/files/out.png";
     // Check if the image is created successfully or not
     if (!image.data) {
