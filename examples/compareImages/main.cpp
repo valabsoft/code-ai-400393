030588210a7df831ebc56b59012cf9cc11a4dfdb
Vasiliy Alchakov <alchakov@gmail.com>
2025-05-07 18:26:47 +0300

    AI-116: Обновлены файлы демонстрационного примера для задачи сравнения изображений

--- examples/compareImages/main.cpp
+++ examples/compareImages/main.cpp
@@ -8,8 +8,8 @@ int main()
 
 	auto imagePath = currentPath / imageFile;
 
-	cv::Mat img1 = cv::imread(imagePath.u8string() + "1.png");
-	cv::Mat img2 = cv::imread(imagePath.u8string() + "2.png");
+	cv::Mat img1 = cv::imread(imagePath.u8string() + "img1.png");
+	cv::Mat img2 = cv::imread(imagePath.u8string() + "img2.png");
 
 	std::cout << "Similarity: " << mrcv::compareImages(img1, img2, 1) << std::endl;
 	return 0;
